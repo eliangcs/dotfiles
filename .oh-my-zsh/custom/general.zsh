ebc655d6182a2e5289267eabca3bb97ed2e5c3cc
Chang-Hung Liang <eliang.cs@gmail.com>
2016-09-13 16:55:51 +0800

    Add world clock to zsh

--- .oh-my-zsh/custom/general.zsh
+++ .oh-my-zsh/custom/general.zsh
@@ -2,3 +2,16 @@ export EDITOR=nvim
 
 export LANG=en_US.utf-8
 export LC_ALL=en_US.utf-8
+
+# World clock
+c() {
+    TZ=America/Los_Angeles date +"%Y-%m-%d %H:%M %z Los Angeles"
+    TZ=America/New_York date +"%Y-%m-%d %H:%M %z New York"
+    TZ=UTC date +"%Y-%m-%d %H:%M %z UTC"
+    TZ=Europe/London date +"%Y-%m-%d %H:%M %z London"
+    TZ=Europe/Berlin date +"%Y-%m-%d %H:%M %z Berlin"
+    TZ=Europe/Moscow date +"%Y-%m-%d %H:%M %z Moscow"
+    TZ=Europe/Taipei date +"%Y-%m-%d %H:%M %z Taipei"
+    TZ=Asia/Tokyo date +"%Y-%m-%d %H:%M %z Tokyo"
+    TZ=Australia/Sydney date +"%Y-%m-%d %H:%M %z Sydney"
+}
